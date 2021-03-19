6f1201012492d6efb5878990c5cba3036c1a98de
princejonc9 <56721554+princejonc9@users.noreply.github.com>
2021-03-19 13:12:01 +0800

    validation schema changed
    -added required property for ref_link

--- middleware/joi/validation.schema.js
+++ middleware/joi/validation.schema.js
@@ -5,7 +5,7 @@ const skillDetailsSchema = Joi.object({
   skill_description: Joi.string().max(100),
   references: Joi.array().items(
     Joi.object({
-      ref_link: Joi.string().max(100),
+      ref_link: Joi.string().max(100).required(),
       ref_category: Joi.number().integer().valid(0,1,2),
       length_in_mins: Joi.number().integer(),
     })
