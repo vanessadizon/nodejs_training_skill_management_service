1d93df6e2b04afa7b44e3a4f0be72261f67e87cb
gerardubuntu <gaabantao1@up.edu.ph>
2021-03-26 18:50:13 +0800

    fixed code smell

--- app/controllers/skill.controller.js
+++ app/controllers/skill.controller.js
@@ -1,32 +1,8 @@
 'use strict';
 
 const skillModel = require('../models/skill.model');
-const Joi = require('@hapi/joi');
-
-const skillSchema = Joi.object({
-    skill_name: Joi.string().min(3).max(50).required(),
-    skill_description: Joi.string().required(),
-    references: Joi.array().items(
-        Joi.object({
-            ref_link: Joi.string().uri(),
-            ref_category: Joi.number(),
-            length_in_mins: Joi.number(),
-        })
-    ),
-});
-
-const idSchema = Joi.string().pattern(/^[0-9]+$/, 'numbers');
-
-class ValidationError extends Error {
-    constructor(message) {
-        // Pass remaining arguments (including vendor specific ones) to parent constructor
-        super(message);
-
-        this.name = 'ValidationError';
-        // Custom debugging information
-        this.code = 'VALIDATION_ERROR';
-    }
-}
+const { ValidationError, errorHandling, callback } = require('../utils/errors');
+const { skillSchema, idSchema } = require('../utils/validation');
 
 // route '/api/v1/aws-training-management-system/skill/id/:skill_id'
 exports.getSkillBySkillId = async (req, res) => {
@@ -34,8 +10,8 @@ exports.getSkillBySkillId = async (req, res) => {
     try {
         const skillIdValidation = idSchema.validate(skill_id);
         if (skillIdValidation.error)
-            throw new ValidationError('skill_id must be an integer');
-        let skillDbResult = await skillModel.getSkillBySkillId(skill_id);
+            throw new ValidationError('skill_id must be numeric');
+        const skillDbResult = await skillModel.getSkillBySkillId(skill_id);
         if (skillDbResult.length > 0) {
             skillDbResult[0].references = await skillModel.getReferenceBySkillId(
                 skill_id
@@ -45,11 +21,7 @@ exports.getSkillBySkillId = async (req, res) => {
             return res.status(200).json({});
         }
     } catch (err) {
-        errorHandling(err, (status_code, error_message) => {
-            return res
-                .status(status_code)
-                .json({ error_message: error_message });
-        });
+        errorHandling(err, callback);
     }
 };
 
@@ -73,11 +45,7 @@ exports.addNewSkill = async (req, res) => {
         });
         return res.status(200).json({ added: '1' });
     } catch (err) {
-        errorHandling(err, (status_code, error_message) => {
-            return res
-                .status(status_code)
-                .json({ error_message: error_message });
-        });
+        errorHandling(err, callback);
     }
 };
 
@@ -87,7 +55,7 @@ exports.updateSkillDetails = async (req, res) => {
         const { skill_id } = req.params;
         const skillIdValidation = idSchema.validate(skill_id);
         if (skillIdValidation.error)
-            throw new ValidationError('skill_id must be an integer');
+            throw new ValidationError('skill_id must be numeric');
 
         const skillValidation = skillSchema.validate(req.body);
         if (skillValidation.error)
@@ -106,11 +74,7 @@ exports.updateSkillDetails = async (req, res) => {
         }
         return res.status(200).json({ updated: '0' });
     } catch (err) {
-        errorHandling(err, (status_code, error_message) => {
-            return res
-                .status(status_code)
-                .json({ error_message: error_message });
-        });
+        errorHandling(err, callback);
     }
 };
 
@@ -120,7 +84,7 @@ exports.deleteSkillBySkillId = async (req, res) => {
     try {
         const skillIdValidation = idSchema.validate(skill_id);
         if (skillIdValidation.error)
-            throw new ValidationError('skill_id must be an integer');
+            throw new ValidationError('skill_id must be numeric');
         const skillDbResult = await skillModel.deleteSkillBySkillId(skill_id);
         if (skillDbResult.affectedRows > 0) {
             await skillModel.deleteReferenceBySkillId(skill_id);
@@ -128,18 +92,14 @@ exports.deleteSkillBySkillId = async (req, res) => {
         }
         return res.status(200).json({ deleted: '0' });
     } catch (err) {
-        errorHandling(err, (status_code, error_message) => {
-            return res
-                .status(status_code)
-                .json({ error_message: error_message });
-        });
+        errorHandling(err, callback);
     }
 };
 
 // route '/api/v1/aws-training-management-system/skill/all'
 exports.getAllSkills = async (req, res) => {
     try {
-        let skillDbResult = await skillModel.getAllSkills();
+        const skillDbResult = await skillModel.getAllSkills();
 
         for (const skill of skillDbResult) {
             skill.references = await skillModel.getReferenceBySkillId(
@@ -148,20 +108,6 @@ exports.getAllSkills = async (req, res) => {
         }
         return res.status(200).json(skillDbResult);
     } catch (err) {
-        errorHandling(err, (status_code, error_message) => {
-            return res
-                .status(status_code)
-                .json({ error_message: error_message });
-        });
-    }
-};
-
-const errorHandling = (err, callback) => {
-    if (err.code === 'ER_DUP_ENTRY') {
-        callback(409, 'Duplicate entry.');
-    } else if (err.code === 'VALIDATION_ERROR') {
-        callback(400, err.message);
-    } else {
-        callback(503, 'Cannot connect to database / System error.');
+        errorHandling(err, callback);
     }
 };
